80c0386f01a32fc1be7c689e0193d31d3c7a6c9c
abdullah abunada <eng.a.abunada@gmail.com>
2017-02-06 11:10:01 +0200

    Add Bottom Navigation Menu

--- app/src/main/java/ps/source/abdullah/masroof/MainActivity.java
+++ app/src/main/java/ps/source/abdullah/masroof/MainActivity.java
@@ -1,8 +1,12 @@
 package ps.source.abdullah.masroof;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -21,6 +25,37 @@ public class MainActivity extends AppCompatActivity {
         //call main app activity layout
         setContentView(R.layout.activity_main);
 
+        //set listener for android bottom nav
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_expenses:
+                        Log.i("screen","1");
+
+                        break;
+
+                    case R.id.action_shopping:
+                        Log.i("screen","2");
+                        break;
+
+                    case R.id.action_saving:
+                        Log.i("screen","3");
+                        break;
+
+
+                    case R.id.action_statistics:
+                        Log.i("screen","4");
+
+                        break;
+
+                }
+                return true;
+            }
+        });
 
     }
 
