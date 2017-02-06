c3a4af554ae3c51bbebb0f9fc662a69a0220bb70
abdullah abunada <eng.a.abunada@gmail.com>
2017-02-06 10:04:56 +0200

    Add Droid Arabic Font & add Support RTL Layout

--- app/src/main/java/ps/source/abdullah/masroof/MainActivity.java
+++ app/src/main/java/ps/source/abdullah/masroof/MainActivity.java
@@ -1,13 +1,31 @@
 package ps.source.abdullah.masroof;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set custom arabic font
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/droid-sans.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+        //call main app activity layout
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
